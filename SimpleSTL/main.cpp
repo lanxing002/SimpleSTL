1b5d8579deb2bf0681a9aad54d9fc3518b0a40c8
scent_wen <>
2020-02-29 10:28:44 +0800

    stash

--- SimpleSTL/main.cpp
+++ SimpleSTL/main.cpp
@@ -4,12 +4,8 @@
 int cau = 42;
 
 int main() {
-	//SimpleSTL::StringTest::testAllCases();
+	SimpleSTL::StringTest::testAllCases();
 	SimpleSTL::VectorTest::testAllCases();
-	//int cau = 142;
-	//{
-	//	std::cout << cau << std::endl;
-	//	std::cout << ::cau << std::endl;
-	//}
+
 	return 0;
 }
